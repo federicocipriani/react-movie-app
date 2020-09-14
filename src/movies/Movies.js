2a370f5bba650d29ddf9df8c246736123307a296
Federico <64685113+federicocipriani@users.noreply.github.com>
2020-09-14 18:43:08 +0100

    Card content exit fixed 140920

--- src/movies/Movies.js
+++ src/movies/Movies.js
@@ -9,41 +9,32 @@ function Movies({ movies }) {
         setExpandedId(expandedId === index ? -1 : index);
     };
 
-    const class_poster = 'movie-poster';
-    const class_poster_active = 'movie-poster movie-poster-active';
-    const class_content = 'card-content';
-    const class_content_active = 'card-content card-content-active';
+    const poster = 'movie-poster';
+    const poster_active = 'movie-poster active';
+    const content = 'card-content';
+    const content_active = 'card-content active';
 
     return (
         <div className='card-list'>
             {movies.map((movie, index) => (
-                <div className='card'>
-                    <div className='card-poster'>
-                        <button
-                            className='clickable-area'
-                            key={movie.id}
-                            onClick={() => handleClick(index)}></button>
-                        <img
-                            className={
-                                expandedId === index
-                                    ? class_poster_active
-                                    : class_poster
-                            }
-                            src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-                            alt=''
-                        />
-                        <div
-                            className={
-                                expandedId === index
-                                    ? class_content_active
-                                    : class_content
-                            }>
-                            <h2 className='movie-title'>{movie.title}</h2>
-                            <p className='movie-release'>
-                                {movie.release_date}
-                            </p>
-                            <p className='movie-overview'>{movie.overview}</p>
-                        </div>
+                <div className='card-container' key={movie.id}>
+                    <button
+                        className='button'
+                        onClick={() => handleClick(index)}></button>
+                    <img
+                        className={
+                            expandedId === index ? poster_active : poster
+                        }
+                        src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+                        alt=''
+                    />
+                    <div
+                        className={
+                            expandedId === index ? content_active : content
+                        }>
+                        <h2 className='movie-title'>{movie.title}</h2>
+                        <p className='movie-release'>{movie.release_date}</p>
+                        <p className='movie-overview'>{movie.overview}</p>
                     </div>
                 </div>
             ))}
